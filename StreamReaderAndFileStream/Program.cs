1929e0888bad420f3a0c0a486cb4b61362f92e8b
Samuel Louis <sampeur4life1@gmail.com>
2020-01-31 00:00:51 -0300

    commit StreamReader and FileStream without using File

---
+++ StreamReaderAndFileStream/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace StreamReaderAndFileStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\sa\source\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured !");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+            Console.ReadLine();
+        }
+    }
+}

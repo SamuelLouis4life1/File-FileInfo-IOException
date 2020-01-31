7b56e9eae6b7ed783e0ec77ff7845ba13fbdeec9
Samuel Louis <sampeur4life1@gmail.com>
2020-01-31 14:48:37 -0300

    commit SreamWriter. A stream capable of writing characters from a binary stream

---
+++ StreamWriterFileStream/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+namespace StreamWriterFileStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcepath = @"C:\Users\sa\source\file1.txt";
+            string targetpath = @"C:\Users\sa\source\file3.txt";
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcepath);
+
+                using (StreamWriter sw = File.AppendText(targetpath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured !");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

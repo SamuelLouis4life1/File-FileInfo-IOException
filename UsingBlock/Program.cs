f00e61720dd734f2254772a97e3456f2e2b81468
Samuel Louis <sampeur4life1@gmail.com>
2020-01-31 00:26:00 -0300

    commit using block

---
+++ UsingBlock/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace UsingBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\sa\source\file1.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured !");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

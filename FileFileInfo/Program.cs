2b242b9168905310a5e4c1c2f7870c98c193bd4a
Samuel Louis <sampeur4life1@gmail.com>
2020-01-30 23:41:53 -0300

    first commit with File, FileInfo and IOException

---
+++ FileFileInfo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace FileFileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\sa\source\file1.txt";
+            string targetPath = @"C:\Users\sa\source\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines) ;
+                Console.WriteLine(lines);  
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured !");
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
